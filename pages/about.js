c48ce33989023b2d592be1b89aa9caa37de98e5b
eric323c <71191021+eric323c@users.noreply.github.com>
2024-09-10 05:03:18 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,11 @@
+// pages/about.js
+export default function About() {
+  return (
+    <div className="container">
+      <h1>About Beyond the Blonde</h1>
+      <p>At Beyond the Blonde, we believe in more than just hair. We create transformations that empower you to be your boldest self. Led by [Stylist Name], our philosophy is all about breaking boundaries and expressing individuality through style.</p>
+      <a href="/" className="cta-button">Back to Home</a>
+    </div>
+  );
+}
 
