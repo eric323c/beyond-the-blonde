84e8d005311f59e3e55a0cc7239fe73edbc1d78b
eric323c <71191021+eric323c@users.noreply.github.com>
2024-09-10 05:43:38 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
