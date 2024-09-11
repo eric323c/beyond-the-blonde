dbc7950804fc815218cb0ef9e7e43d2c8d4e3521
eric323c <71191021+eric323c@users.noreply.github.com>
2024-09-11 13:35:41 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
 import '../styles/globals.css';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
