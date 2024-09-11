04cccdcf3929934246e035455814a50efc39bbb5
eric323c <71191021+eric323c@users.noreply.github.com>
2024-09-11 13:51:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Navbar from '../components/Navbar';
 import { useEffect } from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   useEffect(() => {
-    // Scroll animation trigger for sections
     const sections = document.querySelectorAll('.section');
     const options = { threshold: 0.5 };
 
@@ -27,7 +26,10 @@ export default function Home() {
       {/* Hero Section */}
       <section className="hero-section section">
         <div className="hero-left">
-          <img src="https://jaylophotos.pixieset.com/download/photo/?username=jaylophotos&key=beyondtheblonde&token=yFxGXEXbVIDIEEIC9s2EG4LAN0DZJYGt7cgQyDqWW2Tw5ICbXd8vbN3yWZ6QTZwXOVypIBXHNYamVFdkR1tnSI5wecMkimruwFVRjgYMdT-LW-3GTyftmY5tTospv8vaN4vuC1LzOKhE352Pa4gweOVvbpV-LqipovkGVKlw8ME" alt="Salon" />
+          <img
+            src="https://jaylophotos.pixieset.com/download/photo/?username=jaylophotos&key=beyondtheblonde&token=yFxGXEXbVIDIEEIC9s2EG4LAN0DZJYGt7cgQyDqWW2Tw5ICbXd8vbN3yWZ6QTZwXOVypIBXHNYamVFdkR1tnSI5wecMkimruwFVRjgYMdT-LW-3GTyftmY5tTospv8vaN4vuC1LzOKhE352Pa4gweOVvbpV-LqipovkGVKlw8ME"
+            alt="Salon"
+          />
         </div>
         <div className="hero-right">
           <h1 className="hero-logo">Beyond the Blonde</h1>
@@ -38,7 +40,10 @@ export default function Home() {
       {/* About Us Section */}
       <section className="about-section section">
         <div className="about-left">
-          <img src="https://jaylophotos.pixieset.com/download/photo/?username=jaylophotos&key=beyondtheblonde&token=9Kq5uU-P5sTJYRGS3MDWQXCgBWSE7dR3zOhG_K10VYFxnxIhs_ldJeUNGoQQA-hhvv_aZyTzS8jJceuaxpAOxaS7jiydj0glHPPNkMSTzC8NJGtt15r9jePlAP4aBK-nl50n3VUm3-4tmNC0RgSKwkh5FpewVmNB2a3UJvdKIns" alt="Lexie" />
+          <img
+            src="https://jaylophotos.pixieset.com/download/photo/?username=jaylophotos&key=beyondtheblonde&token=9Kq5uU-P5sTJYRGS3MDWQXCgBWSE7dR3zOhG_K10VYFxnxIhs_ldJeUNGoQQA-hhvv_aZyTzS8jJceuaxpAOxaS7jiydj0glHPPNkMSTzC8NJGtt15r9jePlAP4aBK-nl50n3VUm3-4tmNC0RgSKwkh5FpewVmNB2a3UJvdKIns"
+            alt="Lexie"
+          />
         </div>
         <div className="about-right">
           <h2>About Lexie</h2>
